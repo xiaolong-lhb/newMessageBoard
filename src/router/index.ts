2c6976c1e045f8616bef84fff1c1c42b8f21ae70
xiaolong-lhb <81376718+xiaolong-lhb@users.noreply.github.com>
2023-03-01 00:00:43 +0800

    feat: xx

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+
+      // test
       path: '/',
       name: 'home',
       component: MessageMain
